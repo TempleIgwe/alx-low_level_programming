11895202ffa3e9c0f9be89323270b6d79081bbab
Temple Igwe <temple.igw@gmail.com>
2023-02-16 18:31:58 -0800

    Add 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A function named puts
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
