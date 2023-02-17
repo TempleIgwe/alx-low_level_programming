c16b25593bcdcd4a0ca752c259333537dd44f8b8
Temple Igwe <temple.igw@gmail.com>
2023-02-16 20:35:31 -0800

    Add 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line
+ * Return: 1 (successful)
+ */
+int main(void)
+putts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
