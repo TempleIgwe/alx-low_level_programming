f96024dbd2e80f48283b08e4ad0698ec3dbb5f62
Temple Igwe <temple.igw@gmail.com>
2023-02-16 20:51:46 -0800

    Edit 100-intel file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,9 @@
  */
 int main(void)
 {
-char w;
-int x;
-long int y;
-long long int z;
-float a;
-
-printf("Size of a char:%zu byte(s)\n", (unsigned long)sizeof(w));
-printf("Size of an int:%zu byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a long int:%zu byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a long long int:%zu byte(s)\n", (unsigned long)sizeof(z));
-printf("Size of a float:%zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char:%zu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int:%zu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int:%zu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int:%zu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float:%zu byte(s)\n", (unsigned long)sizeof(float));
 }
